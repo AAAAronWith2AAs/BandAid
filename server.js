2fdcee97bbda0923be32f49112d18e50dfe138f8
Thor138 <tjcourey84@gmail.com>
2021-06-02 10:36:28 -0600

    fixed errors in conncection and server.js

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
+require("dotenv").config();
 
 const sequelize = require("./config/connection");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 1138;
 
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: "Super secret secret",
+  secret: process.env.DB_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
