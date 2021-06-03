35bc72f960f7c57712c48125c81983eff5223d50
Taylor Smith <tbsmith0205@gmail.com>
2021-06-03 17:01:40 -0600

    fixed renders for get alls in route handlers

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -38,6 +38,8 @@ router.get("/", async (req, res) => {
     const userData = await User.findAll({
       include: [{ model: Concert }],
     });
+    const users = userData.map((user) => user.get({ plain: true }));
+    res.render("userDisplay", { users });
     res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
@@ -53,7 +55,7 @@ router.get("/:id", async (req, res) => {
       return;
     }
 
-    const user = concertData.get({ plain: true });
+    const user = userData.get({ plain: true });
     res.render("userDisplay", { user });
 
     res.status(200).json(userData);
