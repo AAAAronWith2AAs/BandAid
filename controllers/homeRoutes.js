9d7c45edb35adc3289aba2a7ba5225d165183830
lapape <lapape07@gmail.com>
2021-06-04 10:34:29 -0600

    removed auth

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,32 +1,43 @@
 const router = require("express").Router();
 const { User } = require("../models");
-const withAuth = require("../utils/auth");
+// const withAuth = require("../utils/auth");
 
-router.get("/", withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render("homepage", {
-      users,
-      logged_in: req.session.logged_in,
-    });
+    const usertData = await User.findAll();
+    const user = userData.map((user) => user.get({ plain: true }));
+    res.render("userDisplay", { user });
+    res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/login", (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
-  }
+// router.get("/", withAuth, async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ["password"] },
+//       order: [["name", "ASC"]],
+//     });
 
-  res.render("login");
-});
+//     const users = userData.map((project) => project.get({ plain: true }));
+
+//     res.render("homepage", {
+//       users,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get("/login", (req, res) => {
+//   if (req.session.logged_in) {
+//     res.redirect("/");
+//     return;
+//   }
+
+//   res.render("login");
+// });
 
 module.exports = router;
