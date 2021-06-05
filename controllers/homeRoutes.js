3a17851990a8842370df9007654fa29d4e50efa3
lapape <lapape07@gmail.com>
2021-06-04 18:13:16 -0600

    added forms to homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,7 +10,30 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+router.get("/newband", async (req, res) => {
+  try {
+    res.status(200);
+    res.render("userForm");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get("/newconcert", async (req, res) => {
+  try {
+    res.status(200);
+    res.render("concertForm");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get("/newvenue", async (req, res) => {
+  try {
+    res.status(200);
+    res.render("venueForm");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // router.get("/", withAuth, async (req, res) => {
 //   try {
 //     const userData = await User.findAll({
