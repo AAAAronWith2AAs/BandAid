e253f56d69e173f74d401e027bb624eee5f73e0a
lapape <lapape07@gmail.com>
2021-06-06 13:23:58 -0600

    renamed main.js to home.js and made new main.js that adds hamburger functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,23 @@
-async function searchFormHandler(event) {
-  event.preventDefault();
-  const category = document.querySelector("#nav-select").value;
-  const name = document.querySelector("#search-value").value;
-  switch (category) {
-    case "Concert by Band":
-      {
-        document.location.replace(`api/users/search/${name}`);
-      }
-      break;
-    case "Venue by Name":
-      {
-        document.location.replace(`api/venues/search/${name}`);
-      }
-      break;
-    case "Band by Name":
-      {
-        document.location.replace(`api/users/search/${name}`);
-      }
-      break;
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
   }
-}
-document
-  .querySelector("#search-button")
-  .addEventListener("click", searchFormHandler);
+});
